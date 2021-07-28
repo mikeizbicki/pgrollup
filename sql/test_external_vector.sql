213a6606721ea21c2135d0d21042b39d3f3f8012
Mike Izbicki <mike@izbicki.me>
2021-07-28 15:44:05 -0700

    improve vector tests

--- sql/test_external_vector.sql
+++ sql/test_external_vector.sql
@@ -2,10 +2,22 @@ SET client_min_messages TO WARNING;
 
 create table vector_test (
     id serial primary key,
-    a vector(3)
+    a vector(3),
+    b integer
 );
 
-insert into vector_test (a) VALUES ('[1,2,3]'), ('[2,3,4]'), (NULL);
+insert into vector_test (a,b) VALUES
+    ('[1,2,3]',0),
+    ('[1,2,3]',0),
+    ('[1,2,3]',1),
+    ('[1,2,3]',NULL),
+    ('[2,3,4]',0),
+    ('[2,3,4]',0),
+    ('[2,3,4]',1),
+    ('[2,3,4]',NULL),
+    (NULL,0),
+    (NULL,1),
+    (NULL,NULL);
 
 CREATE MATERIALIZED VIEW vector_test_rollup1 AS (
     SELECT
@@ -16,10 +28,37 @@ CREATE MATERIALIZED VIEW vector_test_rollup1 AS (
     FROM vector_test
 );
 
-insert into vector_test (a) VALUES ('[1,2,3]'), ('[2,3,4]'), (NULL);
+CREATE MATERIALIZED VIEW vector_test_rollup2 AS (
+    SELECT
+        count(a),
+        vector_sum(a),
+        vector_avg(a),
+        vector_sum(a)/count(a),
+        b
+    FROM vector_test
+    GROUP BY b
+);
 
 select assert_rollup('vector_test_rollup1');
+select assert_rollup('vector_test_rollup2');
+
+insert into vector_test (a,b) VALUES
+    ('[1,2,3]',0),
+    ('[1,2,3]',0),
+    ('[1,2,3]',1),
+    ('[1,2,3]',NULL),
+    ('[2,3,4]',0),
+    ('[2,3,4]',0),
+    ('[2,3,4]',1),
+    ('[2,3,4]',NULL),
+    (NULL,0),
+    (NULL,1),
+    (NULL,NULL);
+
+select assert_rollup('vector_test_rollup1');
+select assert_rollup('vector_test_rollup2');
 
 select * from vector_test_rollup1;
+select * from vector_test_rollup2;
 
 drop table vector_test cascade;
